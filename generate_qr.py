f38f6dc20afe79d2773f044672212701a1cde89c
Andy Wang <wangjiayu_0626@qq.com>
2025-03-26 21:57:30 +0800

    Add files via upload

---
+++ generate_qr.py
@@ -0,0 +1,22 @@
+import qrcode
+
+# Building QR code object
+qr = qrcode.QRCode(
+    version = 1,
+    error_correction = qrcode.constants.ERROR_CORRECT_L,
+    box_size = 10,  
+    border = 4,     
+)
+
+# Add the link
+qr.add_data("https://andypku.github.io/survey/")
+qr.make(fit=True)
+
+# Generate the QR code
+img = qr.make_image(fill_color = "black", back_color = "white")
+
+# Show the code
+img.show()
+
+# Save the code
+img.save("survey_qr.png")
